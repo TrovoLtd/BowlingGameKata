f6a25d2012703aa471ccd13cd62641f05c7277af
ShakeyDave <dave@dgcv.co.uk>
2021-08-31 08:03:23 +0100

    Adds Roll All Ones test.

--- src/BowlingKata/BowlingGame.Tests.Unit/BowlingGameTests.cs
+++ src/BowlingKata/BowlingGame.Tests.Unit/BowlingGameTests.cs
@@ -4,22 +4,38 @@ namespace BowlingGame.Tests.Unit
 {
     public class BowlingGameTests
     {
+        private Game _game;
+
         [SetUp]
         public void Setup()
         {
+            _game = new Game();
         }
 
         [Test]
         public void TestGutterGame()
         {
-            Game game = new Game();
+            RollMany(20, 0);
 
-            for (int i = 0; i < 20; i++)
-            {
-                game.Roll(0); 
-            }
-
-            Assert.AreEqual(0, game.Score);
+            Assert.AreEqual(0, _game.Score);
         }
+
+
+        [Test]
+        public void TestAllOnes()
+        {
+            RollMany(20, 1);
+
+            Assert.AreEqual(20, _game.Score);
+        }
+
+        private void RollMany(int n, int pins)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                _game.Roll(pins);
+            }
+        }
+
     }
 }
